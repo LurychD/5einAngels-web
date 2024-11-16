5b4c77ceda4be2a599e6ab9a737f44dceba90197
Axel Ibarra <tuviejastudios@gmail.com>
2024-11-16 01:03:29 -0300

    General optimization of images. Added LazyLoad script

--- main.js
+++ main.js
@@ -1,3 +1,23 @@
+//Lazysizes optimizacion. Verificacion de funcionamiento
+  //Inicializacion
+    document.addEventListener("DOMContentLoaded", function() {
+      if (typeof LazyLoad !== 'undefined') {
+          console.log("LazyLoad script has loaded successfully!");
+      } else {
+          console.error("LazyLoad script did not load.");
+      }
+    });
+  //Esta cargando la imagen? Imprimir a consola
+  document.addEventListener('lazybeforeunveil', function(e)
+  {
+  console.log('Imagen que está por cargarse:', e.target);
+  });  
+  document.addEventListener('lazyloaded', function(e)
+      {
+      console.log('Imagen cargada:', e.target);
+      });
+  
+
 //Animación de la carta
 const card = document.querySelector('.card');
 let flipped = false;
