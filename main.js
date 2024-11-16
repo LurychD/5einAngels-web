f6e5051ce29b59ec71ec756937b053a1a18327c9
Axel Ibarra <tuviejastudios@gmail.com>
2024-11-16 02:01:56 -0300

    lazyload not referenced anymore

--- main.js
+++ main.js
@@ -1,21 +1,15 @@
-//Lazysizes optimizacion. Verificacion de funcionamiento
-  //Inicializacion
-    document.addEventListener("DOMContentLoaded", function() {
-      if (typeof LazyLoad !== 'undefined') {
-          console.log("LazyLoad script has loaded successfully!");
-      } else {
-          console.error("LazyLoad script did not load.");
-      }
-    });
-  //Esta cargando la imagen? Imprimir a consola
-  document.addEventListener('lazybeforeunveil', function(e)
-  {
-  console.log('Imagen que está por cargarse:', e.target);
-  });  
-  document.addEventListener('lazyloaded', function(e)
-      {
-      console.log('Imagen cargada:', e.target);
-      });
+document.addEventListener('DOMContentLoaded', function() {
+  const imgElement = document.getElementById('responsive-image');
+
+  // Escuchar el evento 'load' para saber cuándo se carga la imagen
+  imgElement.addEventListener('load', function() {
+      // Imprimir el src que ha sido cargado
+      console.log('Imagen cargada:', imgElement.src);
+
+      // Imprimir el srcset y qué imagen está siendo usada
+      console.log('srcset:', imgElement.srcset);
+  });
+});
   
 
 //Animación de la carta
